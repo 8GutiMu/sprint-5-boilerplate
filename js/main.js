34ef592fcb9e1d603d62859c8b8fbf071a56270b
8GutiMu <you@example.com>
2017-06-15 10:01:57 -0500

    agregar los topics a mi table

--- js/main.js
+++ js/main.js
@@ -4,21 +4,29 @@ var api = {
 
 var $topicList = $("#topic-list");
 
-var cargarPagina = function(){
+var cargarPagina = function () {
     cargarTopics();
 };
 
-var cargarTopics = function(){
-    $.getJSON(api.url, function(topic){
+var cargarTopics = function () {
+    $.getJSON(api.url, function (topic) {
         topic.forEach(renderTopic)
     });
 };
 
-var renderTopic = function(topic){
+var renderTopic = function (topic) {
     var author_name = topic.author_name;
     var content = topic.content;
+    var responseConunt = topic.responses_count;
+
+    var $tr = $("<tr />");
+    var $tdTopic = $("<td />")
+    var $tdAuthor = $("<td />")
+    var $spanCount = $("<span />")
     
-    console.log(author_name,"++",content)
+    $tdTopic.text(content);
+    $tdAuthor.text(author_name);
+    $spanCount.text(responseConunt);
 }
 
 $(document).ready(cargarPagina);
