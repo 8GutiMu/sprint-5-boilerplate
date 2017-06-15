61966a480503885bc67b39615e6a1b71f296a66f
8GutiMu <you@example.com>
2017-06-15 11:15:49 -0500

    display de formulario, agregar tipic

--- js/main.js
+++ js/main.js
@@ -4,8 +4,22 @@ var api = {
 
 var $topicList = $("#topic-list");
 
-var cargarPagina = function () {
-    cargarTopics();
+var filterTopics = function (e) {
+    e.preventDefault();
+    var criterioBusqueda = $("#inputFilter").val().toLowerCase();
+
+//    $.getJSON(api.url, function (topics) {
+//         topics.filter(function (topic) {
+//            var nuevos = topic.nombre.toLowerCase().indexOf(criterioBusqueda) >= 0;
+//            return nuevos;
+//        });
+//    });
+
+   
+    console.log(scontactosFiltrados);
+};
+var showForm = function () {
+    $("#topic-form").attr("class", "show-form");
 };
 
 var cargarTopics = function () {
@@ -18,15 +32,60 @@ var renderTopic = function (topic) {
     var author_name = topic.author_name;
     var content = topic.content;
     var responseConunt = topic.responses_count;
+    var id = topic.id;
+    console.log(id)
+
+    //  <tdTopic>Topic <spanAuthor>-por: Bren</span></td>
+    //    <tdResponses>Respuestas: <spanCount>0</span></td>
+
 
     var $tr = $("<tr />");
     var $tdTopic = $("<td />")
-    var $tdAuthor = $("<td />")
+    var $tdResponses = $("<td />")
+    var $spanAuthor = $("<span />")
     var $spanCount = $("<span />")
-    
+
     $tdTopic.text(content);
-    $tdAuthor.text(author_name);
+    $tdResponses.text("Respuestas: ")
+    $spanAuthor.text(" -por: " + author_name);
     $spanCount.text(responseConunt);
+
+    $tr.attr("id", id);
+
+    $tdTopic.append($spanAuthor);
+    $tdResponses.append($spanCount);
+
+    $tr.append($tdTopic);
+    $tr.append($tdResponses);
+
+    $topicList.append($tr);
 }
+
+var addTopic = function (event) {
+    event.preventDefault();
+
+    var authorName = $("#author_name").val();
+    var topicContent = $("#topic_content").val();
+
+
+    $.post(api.url, {
+        author_name: authorName,
+        content: topicContent,
+    }, function (topic) {
+        renderTopic(topic);
+        $("#author_name").text("");
+        $("topic_content").empty;
+        $("#topic-form").removeAttr("class", "show-form");
+        $("#topic-form").attr("class", "hide-form");
+    });
+
+};
+
+var cargarPagina = function () {
+    cargarTopics();
+    $("#topics-form").submit(addTopic);
+    $("#createBtn").click(showForm);
+    $("#inputFilter").submit(filterTopics);
+};
 
 $(document).ready(cargarPagina);
